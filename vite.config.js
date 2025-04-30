76a2c9c28c67c9afdeda06e19d7a14bfaf2ae436
PortalWakai <wakaiportal@gmail.com>
2025-04-29 21:59:25 -0300

    commit

--- vite.config.js
+++ vite.config.js
@@ -4,10 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
-    react(), 'babel-plugin-inline-import',
-    {
-      extensions: ['.svg']
-    }
+    react()
   ],
   base: '/HACKATHON25-PortalWakai/',
   build: {
